80fa0a81b7a5bfc706764b604410ecf51d6247eb
Bernard McKeever <dregin@gmail.com>
2013-01-27 19:15:00 +0000

    Added attributeFilter to only catch one event

--- js/content.js
+++ js/content.js
@@ -18,41 +18,13 @@ mutations.forEach(function(mutation) {
 	var controlDiv = document.getElementById("controlsLeft");
 	var wikiLink = document.createElement("a");
 	wikiLink.class = "share";
-	wikiLink.innerHTML = "<span class=\"normalName\">Wiki Link</span><span class=\"sharedName\">Wiki Link</span><span class=\"placeholderName\">Embedded</span>";
+	wikiLink.innerHTML = "<span class=\"normalName\">Wiki Link</span><span class=\"sharedName\">Wiki Linked</span><span class=\"placeholderName\">Embedded</span>";
 
-	/*
-	var controlDiv = document.getElementById("moreControls");
-
-	var hr = document.createElement("hr");
-	controlDiv.appendChild(hr);
-
-	var content = document.createElement("a");
-	//content.id = "wikiLink";
-	content.class = "share";
-
-	var wikiSpan1 = document.createElement("span");
-	wikiSpan1.class = "normalName";
-	text = document.createTextNode("Wiki Link");
-	wikiSpan1.appendChild(text);
-	content.appendChild(wikiSpan1);
-	
-	var wikiSpan2 = document.createElement("span");
-	wikiSpan2.class = "sharedName";
-	text = document.createTextNode("Wiki Linked");
-	wikiSpan2.appendChild(text);
-	content.appendChild(wikiSpan2);
-
-	var wikiSpan3 = document.createElement("span");
-	wikiSpan3.class = "placeholderName";
-	text = document.createTextNode("Embedded");
-	wikiSpan3.appendChild(text);
-	content.appendChild(wikiSpan3);
-*/
-	//var content = document.createTextNode("<a id=\"wikiLink\" class=\"share\"><span class=\"normalName\">Wiki Link</span><span class=\"sharedName\">Wiki Link</span><span class=\"placeholderName\">Embedded</span><hr></a>");
-	//controlDiv.insertBefore(wikiLink, controlDiv.firstChild);
-	controlDiv.appendChild(wikiLink);
+	controlDiv.insertBefore(wikiLink, document.getElementsByClassName("export"));
+	//controlDiv.appendChild(wikiLink);
  })
 });
 observer.observe(document.getElementById("controlsLeft"), { 	
-	attributes: true
+	attributes: true,
+	attributeFilter: ["class"]
 });
