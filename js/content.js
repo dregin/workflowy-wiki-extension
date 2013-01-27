82e149beaeb0752fb1acab2c294388f847140411
Bernard McKeever <dregin@gmail.com>
2013-01-27 20:02:57 +0000

    Listener Added for click event

--- js/content.js
+++ js/content.js
@@ -1,14 +1,18 @@
 var observer = new WebKitMutationObserver(function(mutations) {
-mutations.forEach(function(mutation) {
-	observer.disconnect();
+	mutations.forEach(function(mutation) {
+		observer.disconnect();
 
-	var controlDiv = document.getElementById("controlsLeft");
-	var wikiLink = document.createElement("a");
-	wikiLink.class = "share";
-	wikiLink.innerHTML = "<span class=\"normalName\">Wiki Link</span><span class=\"sharedName\">Wiki Linked</span><span class=\"placeholderName\">Embedded</span><hr>";
+		var controlDiv = document.getElementById("controlsLeft");
+		var wikiLink = document.createElement("a");
+		wikiLink.class = "share";
+		wikiLink.innerHTML = "<span id=\"wikiLink\" class=\"normalName\">Wiki Link</span><span class=\"sharedName\">Wiki Linked</span><span class=\"placeholderName\">Embedded</span><hr>";
 
-	controlDiv.insertBefore(wikiLink, controlDiv.childNodes[1]);
- })
+		controlDiv.insertBefore(wikiLink, controlDiv.childNodes[1]);
+
+		document.getElementById('wikiLink').addEventListener('click', function() {
+	    	console.log("Wiki Link clicked!");
+			});
+ 	})
 });
 observer.observe(document.getElementById("controlsLeft"), { 	
 	attributes: true,
