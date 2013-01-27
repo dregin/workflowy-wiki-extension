296a180f677d9dcc29016913daafe81f2b1d623c
Bernard McKeever <dregin@gmail.com>
2013-01-27 19:44:52 +0000

    Adding "Wiki Link" item to top of dropdown menu for now

--- js/content.js
+++ js/content.js
@@ -1,27 +1,13 @@
 var observer = new WebKitMutationObserver(function(mutations) {
 mutations.forEach(function(mutation) {
-	/*
-	if(mutation.target.id === "controlsLeft")
-	{
-		var set = false;
-		if (document.getElementById("controlsLeft") === null && set == false){
-			//var controlDiv = document.getElementById("controlsLeft");
-			console.log(mutation);
-			var content = document.createTextNode("<a id=\"wikiLink\" class=\"share\"><span class=\"normalName\">Wiki Link</span><span class=\"sharedName\">Wiki Link</span><span class=\"placeholderName\">Embedded</span><hr></a>");
-			mutation.target.appendChild(content);
-			set = true;
-		}
-	}
-	*/
 	observer.disconnect();
 
 	var controlDiv = document.getElementById("controlsLeft");
 	var wikiLink = document.createElement("a");
 	wikiLink.class = "share";
-	wikiLink.innerHTML = "<span class=\"normalName\">Wiki Link</span><span class=\"sharedName\">Wiki Linked</span><span class=\"placeholderName\">Embedded</span>";
+	wikiLink.innerHTML = "<span class=\"normalName\">Wiki Link</span><span class=\"sharedName\">Wiki Linked</span><span class=\"placeholderName\">Embedded</span><hr>";
 
-	controlDiv.insertBefore(wikiLink, document.getElementsByClassName("export"));
-	//controlDiv.appendChild(wikiLink);
+	controlDiv.insertBefore(wikiLink, controlDiv.childNodes[1]);
  })
 });
 observer.observe(document.getElementById("controlsLeft"), { 	
